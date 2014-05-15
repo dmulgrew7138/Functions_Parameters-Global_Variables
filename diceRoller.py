a9d2efa32a16ab4776970d6fda851c9764593549
dmulgrew7138 <dmulgrew7138@sohamcollege.org.uk>
2014-05-15 13:11:12 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -1,34 +1,37 @@
 #Coin flip program
 #Describe the purpose of this program here.
 
-inport random;time
+import random,time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
 s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
-s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
+s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\nn"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
-def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+def rollnumber():
+    print("rolling.....")
+    global roll
+    roll = random.randint(1,6)
+    
 
 
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+def show_dice(roll):
+    if roll == 1:
+        print(s1)
+    if roll == 2:
         print(s2)
-    else roll = 3
+    if roll == 3:
         print(s3)
-    else roll = 4
+    if roll == 4:
         print(s4)
-    else roll = 5
+    if roll == 5:
         print(s5)
-    else roll = 6
+    if roll == 6:
         print(s6)
 
-roll
-time.sleep(1)
-show_dice(roll)
+
+    rollnumber()
+    time.sleep(1)
+    show_dice(roll)
